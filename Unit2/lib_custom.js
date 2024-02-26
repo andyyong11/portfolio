8c5ec1a4aeae7ecc03c79200fadc121bbd2e7eda
andyyong11 <yongandy07@gmail.com>
2024-02-26 04:44:45 -0600

    23

--- Unit2/lib_custom.js
+++ Unit2/lib_custom.js
@@ -1,1 +1,10 @@
+class MyCustomClass {
+    constructor(name) {
+        this.name = name;
+    }
 
+    logName() {
+        console.log(this.name);
+    }
+}
+console.log("hello");
--- Unit2/object.js
+++ Unit2/object.js
@@ -1,1 +1,13 @@
 
+// Create new instances of MyCustomClass
+let obj1 = new MyCustomClass("Object 1");
+let obj2 = new MyCustomClass("Object 2");
+let obj3 = new MyCustomClass("Object 3");
+
+// Call the logName() method for each object
+obj1.logName();
+obj2.logName();
+obj3.logName();
+
+console.log("hello");
+
