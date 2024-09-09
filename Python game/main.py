67a0f1a6db41e722960a77f762ba22ef5994d541
andyyong11 <yongandy07@gmail.com>
2024-09-09 03:58:13 -0500

    asdf

--- Python game/main.py
+++ Python game/main.py
@@ -13,8 +13,6 @@ pygame.display.set_caption("My board")
 exit = False
 
 while not exit:
-    canvas.fill(color)
-    canvas.blit(image, dest = position)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
